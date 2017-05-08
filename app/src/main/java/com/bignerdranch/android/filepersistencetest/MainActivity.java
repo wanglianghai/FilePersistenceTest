b8710847991e9b4f1ec9939ccb5d3a9c8a6872ca
wanglianghai <1540446221@qq.com>
2017-05-08 14:27:09 +0800

    写入

--- app/src/main/java/com/bignerdranch/android/filepersistencetest/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/filepersistencetest/MainActivity.java
@@ -1,13 +1,41 @@
 package com.bignerdranch.android.filepersistencetest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditText = (EditText) findViewById(R.id.edit);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        String editor = mEditText.getText().toString();
+        save(editor);
+    }
+
+    private void save(String s) {
+        String filename = "myFile";
+        FileOutputStream outputStream;
+        try {
+            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(s.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
