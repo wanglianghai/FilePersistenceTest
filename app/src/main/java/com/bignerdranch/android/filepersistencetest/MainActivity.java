14a8fc2d596f812045d23dac22c6f6fbd64bf281
wanglianghai <1540446221@qq.com>
2017-05-08 15:10:21 +0800

    读取

--- app/src/main/java/com/bignerdranch/android/filepersistencetest/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/filepersistencetest/MainActivity.java
@@ -3,12 +3,19 @@ package com.bignerdranch.android.filepersistencetest;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String FILENAME = "myFile";
     private EditText mEditText;
 
     @Override
@@ -17,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mEditText = (EditText) findViewById(R.id.edit);
+        String editor = load();
+        if (!TextUtils.isEmpty(editor)) {
+            mEditText.setText(load());
+            mEditText.setSelection(editor.length());
+            Toast.makeText(this, "Restoring succeeded", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
@@ -28,14 +41,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void save(String s) {
-        String filename = "myFile";
         FileOutputStream outputStream;
         try {
-            outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream = openFileOutput(FILENAME, Context.MODE_PRIVATE);
             outputStream.write(s.getBytes());
             outputStream.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private String load() {
+        FileInputStream inputStream;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            inputStream = openFileInput(FILENAME);
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
     }
 }
